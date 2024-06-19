93304ff94c1fffdb1428f34606e67be412b35411
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-06-19 17:21:49 +0300

    google login button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,20 @@
 <script>
   import { PUBLIC_BASE_URL } from "$env/static/public";
+  import google from "$lib/assets/google.png";
 </script>
 
 <main>
-  <a href={`${PUBLIC_BASE_URL}/oauth2/authorization/google`}>Sign up with Google</a>
+  <a href={`${PUBLIC_BASE_URL}/oauth2/authorization/google`}>
+    <img width="200px" src={google} alt="google"/>
+  </a>
 </main>
+
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* 100% of the viewport height */
+  }
+</style>
